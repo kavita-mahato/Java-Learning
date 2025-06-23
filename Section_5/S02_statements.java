64e840511b622bd43a8d9abbc3f8fab2fb283501
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-24 04:52:15 +0530

    Statements, Whitespaces and Code organization

---
+++ Section_5/S02_statements.java
@@ -0,0 +1,22 @@
+package Section_5;
+
+public class S02_statements {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("\nThis is a test");
+        System.out.println("This is"
+                + " another" +
+                " Still more ");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("myVariable " + myVariable);
+
+        if(myVariable ==0){
+            System.out.println("It's now zero.");
+        }
+
+    }
+}
