d1d90fc30b5f96aad243e441835aca87558ae763
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-24 18:18:33 +0530

    Challenge 6

--- Section_5/S13_challenge06.java
+++ Section_5/S13_challenge06.java
@@ -8,6 +8,12 @@ package Section_5;
 public class S13_challenge06 {
     public static void main(String[] args) {
         System.out.println("\n");
-        
+        boolean isTrue = areEqualByThreeDecimalPlaces(2.012, 2.012);
+        System.out.println(isTrue);
+    }
+    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        long a = (long)(num1 * 1000);
+        long b = (long)(num2 * 1000);
+        return (a == b);
     }
 }
