42bd423bc5801e0eb274c3a935b008ac8446f167
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-24 23:12:00 +0530

    Bonus challenge

--- Section_5/S18_timeBasedCalculation.java
+++ Section_5/S18_timeBasedCalculation.java
@@ -3,24 +3,30 @@ package Section_5;
 public class S18_timeBasedCalculation {
     public static void main(String[] args) {
         System.err.println("\n");
-        System.err.println(getDurationString(3456));
+        System.err.println(getDurationString(3945));
         System.out.println(getDurationString(65, 45));
 
     }
 
     public static String getDurationString(int seconds) {
-    
-        int minutes = seconds / 60;
-        return getDurationString(minutes,seconds );
+        if (seconds < 0) {
+            return "Invalid data";
+        }
+        return getDurationString(seconds / 60, seconds%60);
 
     }
 
     public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0) {
+            return "Invalid data";
+        }
+        if (seconds < 0 || seconds > 59) {
+            return "Invalid data";
+        }
         int hours = minutes / 60;
 
         int remMin = minutes % 60;
-        int remSec = seconds % 60;
-        return hours + "h " + remMin + "m " + remSec + "s";
+        return hours + "h " + remMin + "m " + seconds + "s";
 
     }
 }
