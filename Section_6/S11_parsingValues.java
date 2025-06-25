324715d7e416f6e3d8aefc5cf8674d4ed3704aae
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-26 01:31:04 +0530

    Parsing

---
+++ Section_6/S11_parsingValues.java
@@ -0,0 +1,21 @@
+package Section_6;
+
+public class S11_parsingValues {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        int currentYear = 2025;
+        System.out.println(getInputFromConsole(currentYear));
+        System.out.println(getInputFromScanner(currentYear));
+
+    }
+    public static String getInputFromConsole(int currentYear) {
+          String name = System.console().readLine("Hi, What's your Name? ");   // Taking inputs
+          System.out.println("Hi " + name + ", Thanks for taking the course!");
+          String dateOfBirth = System.console().readLine("What year were you born? ");   // Taking inputs
+          int age = currentYear - Integer.parseInt(dateOfBirth);  
+          return "So you are " + age + " years old";
+    }
+    public static String getInputFromScanner(int currentYear) {
+          return "";
+    }
+}
