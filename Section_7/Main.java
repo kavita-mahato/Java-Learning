04c81f14691e310d8280185ad519f00e8aabb678
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-27 23:37:50 +0530

    POJO

---
+++ Section_7/Main.java
@@ -0,0 +1,21 @@
+package Section_7;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        for(int i = 1; i<= 5; i++){
+        Student s = new Student("592300" + i,
+        switch(i){
+            case 1 -> "Kavita";
+            case 2 -> "Antara";
+            case 3 -> "Bhagya";
+            case 4 -> "Nahida";
+            case 5 -> "Priyanka";
+            default -> "Anonymous";
+        },
+        "05/11/2000",
+        "Java");
+    System.out.println(s);
+    }
+    }
+}
---
+++ Section_7/S06_POJOs.java
@@ -0,0 +1,7 @@
+package Section_7;
+
+public class S06_POJOs {
+    public static void main(String[] args) {
+        
+    }
+}
