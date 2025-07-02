3375269069af0cfcff6bfcaf4ce6878d85764df8
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-02 13:16:15 +0530

    UPDATE

--- Section_7/S09_workerChallenge.java
+++ Section_7/S09_workerChallenge.java
@@ -7,7 +7,15 @@ public class S09_workerChallenge {
         System.out.println("Age = " + kavita.getAge());
         System.out.println("Pay = " + kavita.collectPay());
 
-        Employee antara = new Employee("Kavita", "11/11/1111", "03/03/2020");
+        SalariedEmployee antara = new SalariedEmployee("Kavita", "11/11/1111", "03/03/2020",35000);
         System.out.println(antara);
+        System.out.println("Antara's paycheck = $" + antara.collectPay());
+        antara.retire();
+        System.out.println("Antara's Pension check = $"+ antara.collectPay());
+        
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970", "03/03/2021", 15);
+        System.out.println(mary);
+        System.out.println("Mary's Paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday pay = $" + mary.getDoublePay());
     }
 }
