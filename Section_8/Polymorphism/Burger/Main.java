1a9fea7f174bbba0236ec43a3c4f60147f667ffe
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-05 19:44:26 +0530

    Master challenge completed

--- Section_8/Polymorphism/Burger/Main.java
+++ Section_8/Polymorphism/Burger/Main.java
@@ -3,12 +3,21 @@ package Polymorphism.Burger;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" ");
-        Item coke = new Item("drink", "coke", 1.50);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
+        // Item coke = new Item("drink", "coke", 1.50);
+        // coke.printItem();
+        // coke.setSize("LARGE");
+        // coke.printItem();
 
-        Item avocado = new Item("Topping", "avocado", 1.50);
-        avocado.printItem();
+        // Item avocado = new Item("Topping", "avocado", 1.50);
+        // avocado.printItem();
+
+    //    Burger burger = new Burger("regular", 4.00);
+    //    burger.addToppings("BACON", "CHEESE", "MAYO");
+    //    burger.printItem();     
+    
+       MealOrder regularMeal = new MealOrder();
+       regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+       regularMeal.setDrinkSize("LARGE");
+       regularMeal.printItemizedList();
     }
 }
