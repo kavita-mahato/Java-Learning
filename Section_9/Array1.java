fa0b0dc19e916cf9b976125922dc3ca47430fdca
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-05 23:19:10 +0530

    Array basics

---
+++ Section_9/Array1.java
@@ -0,0 +1,28 @@
+package Section_9;
+
+public class Array1 {
+    public static void main(String[] args) {
+        System.out.println("\n");
+
+        int[] myIntArray = new int[10];
+        myIntArray[0] = 45;
+        myIntArray[1] = 1;
+        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        myDoubleArray[2] = 3.5;
+        System.out.println(myDoubleArray[2]);
+
+        int[] firstTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("first = " + firstTen[0]);
+        int arrayLength = firstTen.length;
+        System.out.println("length of array = " + arrayLength);
+        System.out.println("last = " + firstTen[arrayLength - 1]);
+
+        int[] newArray;
+        newArray = new int[] {5, 4, 3, 2, 1};
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i] + " ");
+        }
+    }
+}
