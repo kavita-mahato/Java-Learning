ba62476dc81b39f4ed0aef2a926840eb45c8e842
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-05 11:41:12 +0530

    Compile-time and Run time

--- Section_8/Polymorphism/Movie/Main.java
+++ Section_8/Polymorphism/Movie/Main.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n");
-        Movie theMovie = new Adventure("Starwars");
-        theMovie.watchMovie();
-        Movie theCMovie = new Comedy("Hungama");
-        theCMovie.watchMovie();
-        
+        // System.out.println("\n");
+        // Movie theMovie = Movie.getMovie("Comedy", "Kanchana");
+        // theMovie.watchMovie();
+        // Movie theCMovie = new Comedy("Hungama");
+        // theCMovie.watchMovie();
+        // Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter A or C : ");
+            String type = s.nextLine();
+            if("Qq".contains(type)){
+                break;
+            }
+            System.out.print("Enter Movie title : ");
+            String title = s.nextLine();
+            Movie movie = Movie.getMovie(type, title);
+            movie.watchMovie();
+        }
     }
 }
-
-
