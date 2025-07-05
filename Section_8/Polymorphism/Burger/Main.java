e02de0aa389f33720e87bb31a85c8a8eaab6d149
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-05 20:04:47 +0530

    Completed challenge

--- Section_8/Polymorphism/Burger/Main.java
+++ Section_8/Polymorphism/Burger/Main.java
@@ -11,13 +11,20 @@ public class Main {
         // Item avocado = new Item("Topping", "avocado", 1.50);
         // avocado.printItem();
 
-    //    Burger burger = new Burger("regular", 4.00);
-    //    burger.addToppings("BACON", "CHEESE", "MAYO");
-    //    burger.printItem();     
-    
-       MealOrder regularMeal = new MealOrder();
-       regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
-       regularMeal.setDrinkSize("LARGE");
-       regularMeal.printItemizedList();
+        // Burger burger = new Burger("regular", 4.00);
+        // burger.addToppings("BACON", "CHEESE", "MAYO");
+        // burger.printItem();
+
+        // MealOrder regularMeal = new MealOrder();
+        // regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        // regularMeal.setDrinkSize("LARGE");
+        // regularMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up",
+                "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE",
+                "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
     }
 }
