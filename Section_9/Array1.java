56fc592dbd49b5397a9928220dd495ceb6b8b539
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-06 02:24:47 +0530

    Arrays methods

--- Section_9/Array1.java
+++ Section_9/Array1.java
@@ -1,5 +1,7 @@
 package Section_9;
 
+import java.util.Arrays;
+
 public class Array1 {
     public static void main(String[] args) {
         System.out.println("\n");
@@ -20,9 +22,31 @@ public class Array1 {
         System.out.println("last = " + firstTen[arrayLength - 1]);
 
         int[] newArray;
-        newArray = new int[] {5, 4, 3, 2, 1};
+        // newArray = new int[] {5, 4, 3, 2, 1};
+        newArray = new int[5];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = newArray.length - i;
+        }
         for (int i = 0; i < newArray.length; i++) {
             System.out.print(newArray[i] + " ");
         }
+        System.out.println();
+        for (int element : newArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+
+        System.out.println();
+        System.out.println(Arrays.toString(newArray));
+        Object objectVariable = newArray;
+        if (objectVariable instanceof int[]) {
+            System.out.println("objectVariable is really an int array");
+        }
+
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
     }
 }
