9b80b5aa51e887eb067c5bfd72c58473ca99e8f9
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-05 08:08:25 +0530

    polymorphism

---
+++ Section_8/Polymorphism/Movie/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        Movie theMovie = new Adventure("Starwars");
+        theMovie.watchMovie();
+        Movie theCMovie = new Comedy("Hungama");
+        theCMovie.watchMovie();
+        
+    }
+}
+
+
