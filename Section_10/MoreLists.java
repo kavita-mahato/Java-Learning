a8e9b92135a17563db898fe3aba976da28207cc8
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-06 12:28:03 +0530

    Arraylist methods

---
+++ Section_10/MoreLists.java
@@ -0,0 +1,26 @@
+package Section_10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoreLists {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        String[] items = {"apples", "bananas", "milk", "eggs"};
+
+        List<String> list = List.of(items);
+        System.out.println(list);
+
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogurt");
+        System.out.println(groceries);
+
+        ArrayList<String> nextList = new ArrayList<>(
+                List.of("pickles", "mustard", "cheese"));
+        System.out.println(nextList);
+
+        groceries.addAll(nextList);
+        System.out.println(groceries);
+
+    }
+}
