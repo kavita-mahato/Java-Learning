a8c8ec2abbd45c26b42bebbe65f1999aba193cfc
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-07 08:51:41 +0530

    Autoboxing Unboxing

---
+++ Section_10/autoBoxing.java
@@ -0,0 +1,28 @@
+package Section_10;
+
+public class autoBoxing {
+    public static void main(String[] args) {
+        System.out.println("\n");
+
+        Integer boxedInt = Integer.valueOf(15); // preferred but unnecessary
+        Integer deprecatedBoxing = new Integer(15); // deprecated since JDK 9
+        int unboxedInt = boxedInt.intValue(); // unnecessary
+
+        // Automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+        // System.out.println(autoUnboxed.getClass().getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+    }
+
+    private static Double getDoubleObject() {
+        return Double.valueOf(100.00);
+    }
+    private static double getLiteralDoublePrimitive() {
+        return 100.0;
+    }
+
+}
