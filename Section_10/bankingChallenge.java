232da612d1dc03824b54ea64463f73c573a10ef5
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-07 09:42:14 +0530

    Challenge Completed

--- Section_10/bankingChallenge.java
+++ Section_10/bankingChallenge.java
@@ -17,6 +17,18 @@ public class bankingChallenge {
 
         Customer kavita = new Customer("Kavita", 1000.0);
         System.out.println(kavita);
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("jane A", -75.01);
+        bank.printStatement("Jane a");
+
+        bank.addNewCustomer("Kavita", 25);
+        bank.addTransaction("kavita", 100);
+        bank.printStatement("Kavita");
     }
 }
 class Bank{
@@ -26,6 +38,14 @@ class Bank{
     public Bank(String name) {
         this.name = name;
     }
+    
+
+    @Override
+    public String toString() {
+        return "Bank [name=" + name + ", customers=" + customers + "]";
+    }
+
+
     private Customer getCustomer(String customerName) {
 
         for (var customer : customers) {
@@ -44,5 +64,27 @@ class Bank{
             customers.add(customer);
             System.out.println("New Customer added: " + customer);
         }
+    }
+    public void addTransaction(String name, double transactionAmount) {
+
+        Customer customer = getCustomer(name);
+        if (customer != null) {
+            customer.transactions().add(transactionAmount);
+        }
+    }
+    public void printStatement(String customerName) {
+
+        Customer customer = getCustomer(customerName);
+        if (customer == null) {
+            return;
+        }
+
+        System.out.println("-".repeat(30));
+        System.out.println("Customer Name: " + customer.name());
+        System.out.println("Transactions:");
+        for (double d : customer.transactions()) {  // unboxing
+            System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+        }
+
     }
 }
