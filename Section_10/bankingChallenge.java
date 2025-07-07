7f244fa38f91846fd77702e145af4c539faa9d15
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-07 09:32:45 +0530

    Banking app challenge

---
+++ Section_10/bankingChallenge.java
@@ -0,0 +1,48 @@
+package Section_10;
+
+import java.util.ArrayList;
+
+record Customer(String name, ArrayList<Double> transactions) {
+
+    public Customer(String name, double initialDeposit) {
+        this(name.toUpperCase(),
+                new ArrayList<Double>(500));
+        transactions.add(initialDeposit);
+    }
+}
+
+public class bankingChallenge {
+    public static void main(String[] args) {
+        System.out.println("\n");
+
+        Customer kavita = new Customer("Kavita", 1000.0);
+        System.out.println(kavita);
+    }
+}
+class Bank{
+    private String name;
+    private ArrayList<Customer> customers = new ArrayList<>(5000);
+
+    public Bank(String name) {
+        this.name = name;
+    }
+    private Customer getCustomer(String customerName) {
+
+        for (var customer : customers) {
+            if (customer.name().equalsIgnoreCase(customerName)) {
+                return customer;
+            }
+        }
+        System.out.printf("Customer (%s) wasn't found %n", customerName);
+
+        return null;
+    }
+    public void addNewCustomer(String customerName, double initialDeposit) {
+
+        if (getCustomer(customerName) == null) {
+            Customer customer = new Customer(customerName, initialDeposit);
+            customers.add(customer);
+            System.out.println("New Customer added: " + customer);
+        }
+    }
+}
