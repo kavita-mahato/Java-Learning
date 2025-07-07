28b215c8d73850795ea013a8f071b7da08f68a4e
Kavita Mahato <kavitamahato0835@gmail.com>
2025-07-07 09:04:54 +0530

    Working with Wrappers

--- Section_10/autoBoxing.java
+++ Section_10/autoBoxing.java
@@ -1,5 +1,9 @@
 package Section_10;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class autoBoxing {
     public static void main(String[] args) {
         System.out.println("\n");
@@ -16,8 +20,33 @@ public class autoBoxing {
 
         Double resultBoxed = getLiteralDoublePrimitive();
         double resultUnboxed = getDoubleObject();
-    }
 
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+
+        System.out.println(wrapperArray[0].getClass().getName());
+
+        Character[] characterArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var ourList = List.of(1, 2, 3, 4, 5);
+        System.out.println(ourList);
+
+    }
+    private static ArrayList<Integer> getList(Integer... varargs) {
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i : varargs) {
+            aList.add(i);
+        }
+        return aList;
+    }
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+    private static Integer returnAnInteger(int i) {
+        return i;
+    }
     private static Double getDoubleObject() {
         return Double.valueOf(100.00);
     }
